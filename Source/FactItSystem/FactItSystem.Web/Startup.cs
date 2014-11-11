c177cf3201f516b3f797b1c180e53f1b09e007dc
didimitrova <dianidita@gmail.com>
2014-11-12 00:01:08 +0200

    Solution

---
+++ Source/FactItSystem/FactItSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FactItSystem.Web.Startup))]
+namespace FactItSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
